95dd715e2b3b62c6343e0a6634af9768d4bd6dbe
Zhaslan <zhaslantj@gmail.com>
2026-01-24 16:19:50 +0300

    Save work before pull

--- telegrambot_vol_3/main.py
+++ telegrambot_vol_3/main.py
@@ -6,7 +6,7 @@ from main import collect_data
 import os
 import asyncio
 
-bot = Bot(token=os.getenv('7003938041:AAGrohI354MBAprFQflE5-xa7ftor7l7RAw'), parse_mode=types.ParseMode.HTML)
+bot = Bot(token=os.getenv('///'), parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(bot)
 
 
@@ -17,51 +17,51 @@ async def start(message: types.Message):
     keyboard.add(*start_buttons)
 
     await message.answer('Выберите категорию', reply_markup=keyboard)
-#
-#
-# @dp.message_handler(Text(equals='🔪 Ножи'))
-# async def get_discount_knives(message: types.Message):
-#     await message.answer('Please waiting...')
 
-#     collect_data(cat_type=2)
-#
-#     with open('result.json') as file:
-#         data = json.load(file)
-#
-#     for index, item in enumerate(data):
-#         card = f'{hlink(item.get("full_name"), item.get("3d"))}\n' \
-#                f'{hbold("Скидка: ")}{item.get("overprice")}%\n' \
-#                f'{hbold("Цена: ")}${item.get("item_price")}🔥'
-#
-#         if index % 20 == 0:
-#             asyncio.sleep(3)
-#
-#         await message.answer(card)
-#
-#
-# @dp.message_handler(Text(equals='🔫 Снайперские винтовки'))
-# async def get_discount_guns(message: types.Message):
-#     await message.answer('Please waiting...')
-#
-#     collect_data(cat_type=4)
-#
-#     with open('result.json') as file:
-#         data = json.load(file)
-#
-#     for index, item in enumerate(data):
-#         card = f'{hlink(item.get("full_name"), item.get("3d"))}\n' \
-#                f'{hbold("Скидка: ")}{item.get("overprice")}%\n' \
-#                f'{hbold("Цена: ")}${item.get("item_price")}🔥'
-#
-#         if index % 20 == 0:
-#             asyncio.sleep(3)
-#
-#         await message.answer(card)
-#
-#
-# def main():
-#     executor.start_polling(dp)
-#
-#
-# if __name__ == '__main__':
-#     main()
+
+@dp.message_handler(Text(equals='🔪 Ножи'))
+async def get_discount_knives(message: types.Message):
+    await message.answer('Please waiting...')
+
+    collect_data(cat_type=2)
+
+    with open('result.json') as file:
+        data = json.load(file)
+
+    for index, item in enumerate(data):
+        card = f'{hlink(item.get("full_name"), item.get("3d"))}\n' \
+               f'{hbold("Скидка: ")}{item.get("overprice")}%\n' \
+               f'{hbold("Цена: ")}${item.get("item_price")}🔥'
+
+        if index % 20 == 0:
+            asyncio.sleep(3)
+
+        await message.answer(card)
+
+
+@dp.message_handler(Text(equals='🔫 Снайперские винтовки'))
+async def get_discount_guns(message: types.Message):
+    await message.answer('Please waiting...')
+
+    collect_data(cat_type=4)
+
+    with open('result.json') as file:
+        data = json.load(file)
+
+    for index, item in enumerate(data):
+        card = f'{hlink(item.get("full_name"), item.get("3d"))}\n' \
+               f'{hbold("Скидка: ")}{item.get("overprice")}%\n' \
+               f'{hbold("Цена: ")}${item.get("item_price")}🔥'
+
+        if index % 20 == 0:
+            asyncio.sleep(3)
+
+        await message.answer(card)
+
+
+def main():
+    executor.start_polling(dp)
+
+
+if __name__ == '__main__':
+    main()
